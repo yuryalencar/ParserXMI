fe654b5afba34655af26685935903df21e96795d
yuryalencar <yuryalencar@yuryalencar>
2016-10-12 14:21:57 -0300

    @yuryalencar:Modificado somente a main para testar se ainda o Parser está conseguindo ler um diagrama de atividades sem problemas.
    Próxima etapa: Verificar o que falta implementar.

--- src/Aplicacao.java
+++ src/Aplicacao.java
@@ -12,12 +12,20 @@ public class Aplicacao {
     public static void main(String[] args) {
     
             ParserXMI parser = new ParserXMI("/home/yuryalencar/NetBeansProjects/Parser_XMI/src/org/unipampa/arquivoxmi/Activity Diagram Example1.xml");
+            ParserXMI parser1 = new ParserXMI("/home/yuryalencar/NetBeansProjects/Parser_XMI/src/org/unipampa/arquivoxmi/PintarCasa.xml");
             
+            System.out.println("-------------- ACTIVITY DIAGRAM --------------");
             for(UmlDiagram d : parser.getDiagrams()){
                 System.out.println(d.toString());
             }
+            System.out.println("----------------------------------------------");
+            
+            System.out.println("-------------- USE CASE DIAGRAM --------------");
+            for(UmlDiagram d : parser1.getDiagrams()){
+                System.out.println(d.toString());
+            }
+            System.out.println("----------------------------------------------");
     }
-    
     //</editor-fold>
     
 }
